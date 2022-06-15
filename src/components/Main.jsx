4ea5bb202d285e9eb20a81735c81367894d9515f
John Prescott <48197513+daronirl@users.noreply.github.com>
2022-06-14 18:18:34 -0700

    Built part of the UI for the meme generator

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+
+
+function Main() {
+  return (
+    <div className="main--cont flex justify-evenly">
+        <input type="text" />
+        <input type="text" />
+    </div>
+  )
+}
+
+export default Main
---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+
+
+function Nav() {
+  return (
+    <div className="bg-[#672280] flex flex-direction p-4">
+      <img src="./images/troll-face.png" className="img--nav" alt="face"/>
+      <h1 className="ml-3 text-white text-3xl">MemeGenerator</h1>
+      
+    </div>
+  )
+}
+
+export default Nav
